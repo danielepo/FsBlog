634a426a35f9a209af358328c0044710e83d6e12
Rachel Reese <rach@rachelree.se>
2015-09-28 11:25:27 -0400

    Updating to use Azure websites.

--- fsblog.fsx
+++ fsblog.fsx
@@ -169,8 +169,8 @@ Target "GitPublish" (fun _ ->
     CommandHelper.runSimpleGitCommand deploy "add ." |> printfn "%s"
     let cmd = sprintf """commit -a -m "Update generated web site (%s)" """ (DateTime.Now.ToString("dd MMMM yyyy"))
     CommandHelper.runSimpleGitCommand deploy cmd |> printfn "%s"
-    Branches.pull deploy "origin" "gh-pages"
-    Branches.pushBranch deploy "origin" "gh-pages"
+    Branches.pull deploy "origin" "master"
+    Branches.pushBranch deploy "origin" "master"
 )
 
 Target "Install" (fun _ -> 
