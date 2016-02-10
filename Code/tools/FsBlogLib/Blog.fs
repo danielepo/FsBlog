f7e60123ffd00f5eeca1f5f2ade85920cdea7809
Rachel <rachel@jet.com>
2016-02-10 11:32:00 -0500

    Reorganizing directories.

--- Code/tools/FsBlogLib/Blog.fs
+++ Code/tools/FsBlogLib/Blog.fs
@@ -89,6 +89,7 @@ module Blog =
             Literate.ProcessMarkdown(fsx.FileName, template, html.FileName, ?prefix=prefix)
           let processed = File.ReadAllText(html.FileName)
           File.WriteAllText(html.FileName, header + processed)
+          printfn "target: %s" target
           printfn "directory name: %s" <| Path.GetDirectoryName(target)
           EnsureDirectory(Path.GetDirectoryName(target))
           razor.ProcessFile(html.FileName)
